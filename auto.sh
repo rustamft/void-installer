88d56ddf31c88508d40180345511405abd84bbc9
Rustam Tazyrov <81812929+rustamft@users.noreply.github.com>
2024-12-08 01:57:20 +0500

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -25,9 +25,9 @@ p # print the in-memory partition table
 w # write the partition table
 q # and we're done
 EOF
-mkfs.vfat /dev/$disk1
-mkfs.ext2 /dev/$disk2
-cryptsetup luksOpen /dev/$disk3
+mkfs.vfat /dev/${disk}1
+mkfs.ext2 /dev/${disk}2
+cryptsetup luksOpen /dev/${disk}3
 cryptsetup luksFormat /dev/mapper/cryptroot
 void-installer
 mount /dev/mapper/cryptroot /mnt
@@ -35,8 +35,8 @@ mount /dev/sda2 /mnt/boot
 mount /dev/sda1 /mnt/boot/efi
 xchroot /mnt
 uuid=$(blkid -o value -s UUID /dev/mapper/cryptroot)
-appendix="rd.auto=1 rd.luks.name=$uuid=cryptroot rd.luks.allow-discards"
-sed -i 's/^GRUB_CMDLINE_LINUX_DEFAULT="[^"]*/& $appendix/' /etc/default/grub
+appendix="rd.auto=1 rd.luks.name=${uuid}=cryptroot rd.luks.allow-discards"
+sed -i 's/^GRUB_CMDLINE_LINUX_DEFAULT="[^"]*/& ${appendix}/' /etc/default/grub
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --boot-directory=/boot
 grub-mkconfig -o /boot/grub/grub.cfg
 xbps-reconfigure -fa
