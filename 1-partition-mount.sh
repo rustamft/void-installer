9d0df3ed3afaf5f065e6231ec3d529a6f8997844
Rustam Tazyrov <81812929+rustamft@users.noreply.github.com>
2024-12-08 17:13:38 +0500

    Update and rename auto.sh to 1-partition-mount.sh

--- 1-partition-mount.sh
+++ 1-partition-mount.sh
@@ -31,15 +31,5 @@ mount /dev/mapper/cryptroot /mnt
 mkdir -p /mnt/boot/efi
 mount /dev/${disk}2 /mnt/boot
 mount /dev/${disk}1 /mnt/boot/efi
-void-installer
-xchroot /mnt /bin/bash << EOF
-uuid=$(blkid -o value -s UUID /dev/mapper/cryptroot)
-appendix="rd.auto=1 rd.luks.name=${uuid}=cryptroot rd.luks.allow-discards"
-sed -i 's/^GRUB_CMDLINE_LINUX_DEFAULT="[^"]*/& ${appendix}/' /etc/default/grub
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --boot-directory=/boot
-grub-mkconfig -o /boot/grub/grub.cfg
-xbps-reconfigure -fa
-exit
-EOF
-umount -R /mnt
-echo "Void Linux installed!"
+echo "Disk has been partitioned and mounted:"
+echo "$(lsblk)"
