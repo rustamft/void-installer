8faac63f4a2605a6596c57d659c883124b11200d
Rustam Tazyrov <81812929+rustamft@users.noreply.github.com>
2024-12-08 17:05:30 +0500

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -22,16 +22,17 @@ p
 w
 q
 EOF
-mkfs.vfat /dev/${disk}1
-mkfs.ext2 /dev/${disk}2
 cryptsetup luksOpen /dev/${disk}3
 cryptsetup luksFormat /dev/mapper/cryptroot
-void-installer
+mkfs.vfat /dev/${disk}1
+mkfs.ext2 /dev/${disk}2
+mkfs.f2fs /dev/mapper/cryptroot
 mount /dev/mapper/cryptroot /mnt
 mkdir -p /mnt/boot/efi
 mount /dev/${disk}2 /mnt/boot
 mount /dev/${disk}1 /mnt/boot/efi
-xchroot /mnt
+void-installer
+xchroot /mnt /bin/bash << EOF
 uuid=$(blkid -o value -s UUID /dev/mapper/cryptroot)
 appendix="rd.auto=1 rd.luks.name=${uuid}=cryptroot rd.luks.allow-discards"
 sed -i 's/^GRUB_CMDLINE_LINUX_DEFAULT="[^"]*/& ${appendix}/' /etc/default/grub
@@ -39,5 +40,6 @@ grub-install --target=x86_64-efi --efi-directory=/boot/efi --boot-directory=/boo
 grub-mkconfig -o /boot/grub/grub.cfg
 xbps-reconfigure -fa
 exit
+EOF
 umount -R /mnt
 echo "Void Linux installed!"
