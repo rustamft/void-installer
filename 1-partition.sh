2c15d783bdef9627afd380100c664931bcd054f7
Rustam Tazyrov <81812929+rustamft@users.noreply.github.com>
2024-12-08 17:57:33 +0500

    Update and rename 1-partition-mount.sh to 1-partition.sh

--- 1-partition.sh
+++ 1-partition.sh
@@ -24,12 +24,5 @@ q
 EOF
 cryptsetup luksFormat /dev/${disk}3
 cryptsetup luksOpen /dev/${disk}3 cryptroot
-mkfs.vfat /dev/${disk}1
-mkfs.ext2 /dev/${disk}2
-mkfs.f2fs /dev/mapper/cryptroot
-mkdir -p /mnt/target/boot/efi
-mount /dev/mapper/cryptroot /mnt/target
-mount /dev/${disk}2 /mnt/target/boot
-mount /dev/${disk}1 /mnt/target/boot/efi
-echo "Disk has been partitioned and mounted:"
+echo "Disk has been partitioned:"
 echo "$(lsblk)"
