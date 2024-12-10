85f18443fa852da1073988114ac16961a38ad075
Rustam Tazyrov <81812929+rustamft@users.noreply.github.com>
2024-12-10 12:39:47 +0500

    Update 1-partition.sh

--- 1-partition.sh
+++ 1-partition.sh
@@ -4,13 +4,35 @@ echo "$(lsblk)"
 while [ -z $disk ] || [ ! -e /dev/$disk ]; do
   read -p "Enter a valid disk name (e.g. sda): " disk
 done
-while [ -z $password ]; do
+while [ -z $password ] || [ $password != $password_confirmation ]; do
   read -s -p "Enter a password for the ${disk}3 partition encryption: " password
+  read -s -p "Please repeat to confirm: " password_confirmation
 done
 fdisk /dev/$disk << EOF
 g
 n
 1
+  read -p "Enter a valid disk name (e.g. sda): " disk
+done
+while [ -z $password ]; do
+  read -s -p "Enter a password for the ${disk}3 partition encryption: " password
+  read -s -p "Please repeat to confirm: " password_confirmation
+done
+fdisk /dev/$disk << EOF
+g
+n
+1
+
++500M
+n
+2
+
++500M
+n
+3
+
+
+w
 
 +500M
 n
