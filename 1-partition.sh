b54e690228dd943c551d25af91e707caad94fb78
Rustam Tazyrov <81812929+rustamft@users.noreply.github.com>
2024-12-10 12:44:03 +0500

    Update 1-partition.sh

--- 1-partition.sh
+++ 1-partition.sh
@@ -6,7 +6,9 @@ while [ -z $disk ] || [ ! -e /dev/$disk ]; do
 done
 while [ -z $password ] || [ $password != $password_confirmation ]; do
   read -s -p "Enter a password for the ${disk}3 partition encryption: " password
+  printf "\n"
   read -s -p "Please repeat to confirm: " password_confirmation
+  printf "\n"
 done
 fdisk /dev/$disk << EOF
 g
@@ -48,5 +50,5 @@ q
 EOF
 echo -n $password | cryptsetup luksFormat /dev/${disk}3 -
 echo -n $password | cryptsetup luksOpen /dev/${disk}3 cryptroot -
-echo "Disk has been partitioned:"
+printf "Disk has been partitioned:\n"
 echo "$(lsblk)"
