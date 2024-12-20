345848059b35a63e59ca17041884a47b09182b35
Rustam Tazyrov <81812929+rustamft@users.noreply.github.com>
2024-12-21 00:52:43 +0500

    Update 3-de.sh

--- 3-de.sh
+++ 3-de.sh
@@ -20,11 +20,16 @@ while [ -z $desktop_environment ]; do
   esac
 done
 xbps-install -Sy dbus NetworkManager bluez tlp pipewire elogind mesa-dri wget flatpak
-rm /etc/runit/runsvdir/default/dhcpcd
-ln -s /etc/sv/dbus /etc/runit/runsvdir/default/
-ln -s /etc/sv/NetworkManager /etc/runit/runsvdir/default/
-ln -s /etc/sv/bluetoothd /etc/runit/runsvdir/default/
-ln -s /etc/sv/tlp /etc/runit/runsvdir/default/
+ln -s /etc/sv/dbus /var/service
+sv up dbus
+sv down dhcpcd
+rm /var/service/dhcpcd
+ln -s /etc/sv/NetworkManager /var/service
+sv up NetworkManager
+ln -s /etc/sv/bluetoothd /var/service
+sv up bluetoothd
+ln -s /etc/sv/tlp /var/service
+sv up tlp
 mkdir -p /etc/pipewire/pipewire.conf.d
 ln -s /usr/share/examples/wireplumber/10-wireplumber.conf /etc/pipewire/pipewire.conf.d/
 mkdir -p /home/${username}/.config/autostart
@@ -32,20 +37,22 @@ ln -s /user/share/applications/pipewire.desktop /home/${username}/.config/autost
 mkdir /etc/sv/backlight
 wget https://raw.githubusercontent.com/rustamft/void-installer/refs/heads/main/backlight/finish -O /etc/sv/backlight/finish
 wget https://raw.githubusercontent.com/rustamft/void-installer/refs/heads/main/backlight/run -O /etc/sv/backlight/run
-ln -s /etc/sv/backlight /etc/runit/runsvdir/default/
+ln -s /etc/sv/backlight /var/service
+sv up backlight
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 case $desktop_environment in
   "GNOME")
     xbps-install -Sy gdm gnome-core xdg-desktop-portal-gnome xdg-user-dirs nautilus file-roller alacritty
-    ln -s /etc/sv/gdm /etc/runit/runsvdir/default/
+    ln -s /etc/sv/gdm /var/service
+    sv up gdm
     ;;
   "KDE")
     xbps-install -Sy sddm plasma-desktop xorg-minimal xdg-desktop-portal-kde xdg-user-dirs pcmanfm-qt ark alacritty
-    ln -s /etc/sv/sddm /etc/runit/runsvdir/default/
+    ln -s /etc/sv/sddm /var/service
+    sv up sddm
     ;;
   *)
     printf '\nInstallation failed'
     exit
     ;;
 esac
-reboot now
