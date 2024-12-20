ce0bd1ecd109a46ea1512f18d9da6d4cdc39389a
Rustam Tazyrov <81812929+rustamft@users.noreply.github.com>
2024-12-20 22:53:58 +0500

    Update 2-boot.sh

--- 2-boot.sh
+++ 2-boot.sh
@@ -9,13 +9,12 @@ mount /dev/${disk}2 /mnt/boot
 mount /dev/${disk}1 /mnt/boot/efi
 xchroot /mnt /bin/bash << EOF
   xbps-install -yRs void-repo-nonfree
-  xbps-install -Sy cryptsetup
+  xbps-install -Sy cryptsetup zramen wget
   uuid=$(blkid -o value -s UUID /dev/mapper/cryptroot)
   appendix="rd.auto=1 rd.luks.name=\${uuid}=cryptroot rd.luks.allow-discards"
   sed -i "s/^GRUB_CMDLINE_LINUX_DEFAULT=\"[^\"]*/& \${appendix}/" /etc/default/grub
   grub-install --target=x86_64-efi --efi-directory=/boot/efi --boot-directory=/boot
   xbps-reconfigure -fa
-  xbps-install -Sy zramen
   echo "zramen -a zstd -n 6 -s 50 -p 100 make" >> /etc/rc.local
   exit
 EOF
