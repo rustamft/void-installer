8263fa2590111e27ee33a19b7881680a3c167506
Rustam Tazyrov <81812929+rustamft@users.noreply.github.com>
2024-12-22 01:43:49 +0500

    Update 3-de.sh

--- 3-de.sh
+++ 3-de.sh
@@ -20,28 +20,28 @@ while [ -z $desktop_environment ]; do
   esac
 done
 xbps-install -Sy dbus NetworkManager bluez tlp pipewire elogind mesa-dri wget flatpak
-ln -s /etc/sv/dbus /var/service
+ln -sf /etc/sv/dbus /var/service
 rm /var/service/dhcpcd
-ln -s /etc/sv/NetworkManager /var/service
-ln -s /etc/sv/bluetoothd /var/service
-ln -s /etc/sv/tlp /var/service
+ln -sf /etc/sv/NetworkManager /var/service
+ln -sf /etc/sv/bluetoothd /var/service
+ln -sf /etc/sv/tlp /var/service
 mkdir -p /etc/pipewire/pipewire.conf.d
-ln -s /usr/share/examples/wireplumber/10-wireplumber.conf /etc/pipewire/pipewire.conf.d/
+ln -sf /usr/share/examples/wireplumber/10-wireplumber.conf /etc/pipewire/pipewire.conf.d/
 mkdir -p /home/${username}/.config/autostart
-ln -s /user/share/applications/pipewire.desktop /home/${username}/.config/autostart
+ln -sf /user/share/applications/pipewire.desktop /home/${username}/.config/autostart
 mkdir /etc/sv/backlight
 wget https://raw.githubusercontent.com/rustamft/void-installer/refs/heads/main/backlight/finish -O /etc/sv/backlight/finish
 wget https://raw.githubusercontent.com/rustamft/void-installer/refs/heads/main/backlight/run -O /etc/sv/backlight/run
-ln -s /etc/sv/backlight /var/service
+ln -sf /etc/sv/backlight /var/service
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 case $desktop_environment in
   "GNOME")
     xbps-install -Sy gdm gnome-core xdg-desktop-portal-gnome xdg-user-dirs nautilus file-roller alacritty
-    ln -s /etc/sv/gdm /var/service
+    ln -sf /etc/sv/gdm /var/service
     ;;
   "KDE")
     xbps-install -Sy sddm sddm-kcm plasma-desktop xorg-minimal xdg-desktop-portal-kde xdg-user-dirs pcmanfm-qt ark alacritty
-    ln -s /etc/sv/sddm /var/service
+    ln -sf /etc/sv/sddm /var/service
     ;;
   *)
     printf '\nInstallation failed'
