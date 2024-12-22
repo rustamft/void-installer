3da437b49dca0863bf57c759da821fdf82f5cdd6
Rustam Tazyrov <81812929+rustamft@users.noreply.github.com>
2024-12-22 20:15:44 +0500

    Update 3-de.sh

--- 3-de.sh
+++ 3-de.sh
@@ -34,6 +34,7 @@ mkdir -p /etc/pipewire/pipewire.conf.d
 ln -sf /usr/share/examples/wireplumber/10-wireplumber.conf /etc/pipewire/pipewire.conf.d/
 mkdir -p ~/.config/autostart
 ln -sf /user/share/applications/pipewire.desktop ~/.config/autostart
+ln -sf /user/share/applications/pipewire-pulse.desktop ~/.config/autostart
 mkdir /etc/sv/backlight
 wget https://raw.githubusercontent.com/rustamft/void-installer/refs/heads/main/backlight/finish -O /etc/sv/backlight/finish
 wget https://raw.githubusercontent.com/rustamft/void-installer/refs/heads/main/backlight/run -O /etc/sv/backlight/run
