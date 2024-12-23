966d95a286fddadd942e5e702b7ff95217fc032a
Rustam Tazyrov <81812929+rustamft@users.noreply.github.com>
2024-12-23 23:08:28 +0500

    Update 1-disk.sh

--- 1-disk.sh
+++ 1-disk.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
-echo "##################################################"
-echo "###                                            ###"
-echo "###   Wellcome to the Disk Partition script!   ###"
-echo "###                                            ###"
-echo "##################################################"
+echo "#################################################################"
+echo "###                                                           ###"
+echo "###          Wellcome to the Disk Partition script!           ###"
+echo "###                                                           ###"
+echo "###                         WARNING!                          ###"
+echo "###   The script will destroy all data on a disk you choose   ###"
+echo "###                                                           ###"
+echo "#################################################################"
 echo "Your current block devices:"
 lsblk
-while [ -z $disk ] || [ ! -e /dev/$disk ]; do
-  read -p "Enter a valid disk name (e.g. sda): " disk
+while [[ -z $disk ]] || [[ ! -e /dev/$disk ]]; do
+  read -p "Enter a valid disk name (e.g. sda or nvme0n1): " disk
   printf "\n"
 done
-while [ -z $password ] || [ $password != $password_confirmation ]; do
-  read -s -p "Enter a password for the ${disk}3 partition encryption: " password
+if [[ $disk == *"nvme"* ]]; then
+  disk_partition_3="${disk}p3"
+else
+  disk_partition_3="${disk}3"
+fi
+while [[ -z $password ]] || [[ $password != $password_confirmation ]]; do
+  read -s -p "Enter a password for the ${disk_partition_3} partition encryption: " password
   printf "\n"
   read -s -p "Please repeat to confirm: " password_confirmation
   printf "\n"
@@ -34,7 +42,7 @@ n
 w
 q
 EOF
-echo -n $password | cryptsetup luksFormat /dev/${disk}3 -
-echo -n $password | cryptsetup luksOpen /dev/${disk}3 cryptroot -
+echo -n $password | cryptsetup luksFormat /dev/$disk_partition_3 -
+echo -n $password | cryptsetup luksOpen /dev/$disk_partition_3 cryptroot -
 printf "\nDisk has been partitioned:\n"
 lsblk
